3e183a5a3642fbf083a5886e352df46b338398aa
jakobadel <44985518+jakobadel@users.noreply.github.com>
2022-03-15 19:32:57 -0400

    Delete lab2 directory

--- lab2/kotlinChatter/app/src/main/java/edu/umich/jakoba/kotlinChatter/Chatt.kt
+++
@@ -1,7 +0,0 @@
-package edu.umich.jakoba.kotlinChatter
-
-class Chatt(var username: String? = null,
-            var message: String? = null,
-            var timestamp: String? = null,
-            var geodata: GeoData? = null)
-{}
--- lab2/kotlinChatter/app/src/main/java/edu/umich/jakoba/kotlinChatter/GeoData.kt
+++
@@ -1,4 +0,0 @@
-package edu.umich.jakoba.kotlinChatter
-
-class GeoData(var lat: Double = 0.0, var lon: Double = 0.0, var loc: String = "",
-              var facing: String = "unknown", var speed: String = "unknown")
